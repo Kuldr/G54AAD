686556e2fd32b21aa09b2ef58631e653bcf5c75f
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-02 17:33:03 +0100

    insertion sort

--- Lab01.py
+++ Lab01.py
@@ -22,7 +22,7 @@ bisect.insort(d, el)
 print(d)
 
 # Task 2 - Selection Sort ------------------------------------------------------
-# Create a random list of 100 ints between 0 and 10
+# Create a random list of 10 ints between 0 and 100
 r = random.sample(range(1,100),10)
 print(r)
 
@@ -34,8 +34,27 @@ for j in range(0, len(r)):
         if( r[i] < r[minIndex] ):
             minIndex = i
     if( minIndex != j ):
-        r[j],r[minIndex]=r[minIndex],r[j]
+        r[j],r[minIndex] = r[minIndex],r[j]
 print(r)
 
 # Task 3 - Insertion Sort ------------------------------------------------------
-# Create a random list of 100 ints between 0 and 10
+# Create a random list of 10 ints between 0 and 100
+r = random.sample(range(1,100),10)
+print(r)
+
+for i in range(1, len(r)):
+    j = i
+    for j in range(i, 0, -1):
+        if( r[j-1] > r[j] ):
+            r[j],r[j-1] = r[j-1],r[j]
+print(r)            
+
+# i ← 1
+# while i < length(A)
+#     j ← i
+#     while j > 0 and A[j-1] > A[j]
+#         swap A[j] and A[j-1]
+#         j ← j - 1
+#     end while
+#     i ← i + 1
+# end while
