06e4f413e99e045ad6d207a18e1bc303dece0a39
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-09 15:10:20 +0100

    Added the O(n^2) method

--- Lab02.py
+++ Lab02.py
@@ -1,5 +1,6 @@
 import random
 import time
+import sys
 
 
 # Meh Method but it could be useful
@@ -8,19 +9,11 @@ import time
 # print(timeit.timeit(stupidMaxSubArray, number=100000)/100000)
 
 # Task 3 - Max Sub Array -------------------------------------------------------
-sizeOfList = 10**1
+sizeOfList = 10**3
 randomList = random.sample(range(-(sizeOfList*10),(sizeOfList*10)),sizeOfList)
 
 
-# Last weeks implementation
-givenList = [13, -3, -25, -20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-
-# The number of sub arrays is the triangle number relating to the length of the list
-# This naive approach runs in O(n^3) time as it takes n time to calcualate the sumSubArray
-#       and it takes n^2 time to run findSubarrays.
-#       There is also time to sort the list at the end but that could run in a linear sweep theorecitcally
-#           We could also keep track the maximum while we go too so that the whole thing is only n^3
-
+# Last weeks implementation ----------------------------------------------------
 def sumSubArray(list, intial, final):
     sum = 0
     for i in range(intial, final+1):
@@ -45,3 +38,27 @@ start = time.time()
 print(stupidMaxSubArray())
 end = time.time()
 print(end - start)
+
+# O(n^2) implementation --------------------------------------------------------
+def betterMaxSubArray():
+    array = randomList
+    imax = jmax = 0
+    maxSum = - sys.maxsize - 1 # -∞
+
+    for i in range(0, len(array)):
+        parSum = 0
+        for j in range(i, len(array)):
+            parSum = parSum + array[j]
+            if parSum > maxSum:
+                maxSum = parSum
+                imax, jmax = i, j
+
+    return(imax, jmax, sum(array[imax:jmax+1]))
+
+print("\nBetter Max Sub Array")
+start = time.time()
+print(betterMaxSubArray())
+end = time.time()
+print(end - start)
+
+# Divide and Conquer implementation --------------------------------------------
