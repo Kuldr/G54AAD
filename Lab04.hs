2f45948450bf4773e178eea38eb2fb59a3965a59
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-23 14:56:30 +0100

    written insert for RB Tree (need to balance)

--- Lab04.hs
+++ Lab04.hs
@@ -9,3 +9,12 @@ searchRBTree Nil _ = False
 searchRBTree (Node _ x lTree rTree) y | x == y = True
                                       | y <= x = searchRBTree lTree y
                                       | y > x  = searchRBTree rTree y
+
+insertRBTree :: RBTree -> Int -> RBTree
+insertRBTree Nil y = Node Red y Nil Nil
+insertRBTree (Node col x lTree rTree) y | y == x = Node col x lTree rTree
+                                        | y <  x = balance $ Node col x (insertRBTree lTree y) rTree
+                                        | y >  x = balance $ Node col x lTree (insertRBTree rTree y)
+
+balance :: RBTree -> RBTree
+balance t = t
